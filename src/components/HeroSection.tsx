6ed9e721c4a81a740948399b7409862bab5c0368
jigyasacsirneist-pixel <jigyasacsirneist@gmail.com>
2026-01-20 15:42:21 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-import heroImage from "@/assets/hero-lab.jpg";
+import heroImage from "@/assets/bannerhero.jpg";
 
 const HeroSection = () => {
   return (
